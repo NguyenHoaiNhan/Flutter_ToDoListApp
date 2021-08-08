98791832bc929ffd91e9b6335f9137201a8b6b4a
nguyenhoainhan <nhan.nguyenhoai9648@gmail.com>
2021-08-08 19:59:18 +0700

    add assets dir

--- lib/view/walkthrough.dart
+++ lib/view/walkthrough.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
-void main() => runApp(StartUpPage());
 
 class StartUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.end,
-      children: [SvgPicture.asset('assets/images/Fill_1.svg'), Text('aking')],
+      children: [Image(image: AssetImage('assets/images/Fill_1.svg'))],
     );
   }
 }
